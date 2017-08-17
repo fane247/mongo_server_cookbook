e9a7aa801aef79f3432164260282d96db4b1452b
Fane <fane247@gmail.com>
2017-08-17 12:13:41 +0100

    added recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,39 @@
 # Recipe:: default
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+
+
+
+apt_repository 'mongodb-org' do
+	components ["multiverse"]
+	distribution "xenial/mongodb-org/3.2"
+	keyserver "hkp://keyserver.ubuntu.com:80"
+	key "EA312927"
+	uri "http://repo.mongodb.org/apt/ubuntu"
+end
+
+package 'mongodb-org'
+
+template "/lib/systemd/system/mongod.service" do
+	source 'mongod.service.erb'
+	owner 'root'
+	group 'root'
+	mode '0755'
+	notifies :restart, 'service[mongod]'
+end
+
+template "/etc/mongod.conf" do
+	source 'mongod.conf.erb'
+	owner 'root'
+	group 'root'
+	mode '0755'
+	notifies :restart, 'service[mongod]'
+end
+
+service "mongod" do
+	action [:start, :enable]
+	supports start: true, restart: true, reload: true
+end
+
+
+
